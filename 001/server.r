cb022735478c6c8b83c1d8b7384d2625b3d8b115
Solanostat <fqshistat@gmail.com>
2015-09-01 13:53:52 -0700

    Update server.r

--- 001/server.r
+++ 001/server.r
@@ -1,24 +1,38 @@
 library(shiny)
-
-#import iris data
+#library(datasets)
 iris = read.table("/Users/Kevin/Downloads/shiny/iris.txt", sep=",") 
-names(iris)<- c("sepal length", "sepal width", "petal length", "petal width","class")
+names(iris)<- c("sepal_length", "sepal_width", "petal_length", "petal_width","class")
+sepal_length = iris[,1]
+sepal_width = iris[,2]
+petal_width = iris[,4]
 
-# Define server logic required to draw a histogram
+# Define server logic required to summarize and view the selected
+# dataset
 shinyServer(function(input, output) {
-
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-
-  output$distPlot <- renderPlot({
-    x    <- iris[,2]  # iris data
+  
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "sepal_length" = data.frame(sepal_length),
+           "sepal_width"  = data.frame(sepal_width),
+           "petal_width"  = data.frame(petal_width) )
+  })
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  # Show the first "n" observations
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+    output$distPlot <- renderPlot({
+    x <- as.matrix( datasetInput()) # iris data
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
+ 
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="Histogram of sepal_width")
+    hist(x, breaks = bins, col = 'darkgray', border = 'white', main="Histogram of selected column")
   })
 })
