dd560cfecddf3211442c89de331f943f1caef526
Solanostat <fqshistat@gmail.com>
2015-09-01 13:54:27 -0700

    Update ui.r

--- 001/ui.r
+++ 001/ui.r
@@ -1,21 +1,29 @@
-# Define UI for application that draws a histogram
+library(shiny)
+
+# Define UI for dataset viewer application
 shinyUI(fluidPage(
-
+  
   # Application title
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar with a slider input for the number of bins
+  titlePanel("Shiny Iris"),
+  
+  # Sidebar with controls to select a dataset and specify the
+  # number of observations to view
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      selectInput("dataset", "Choose a dataset:", 
+                  choices = c("sepal_length", "sepal_width", "petal_width")),
+      
+      numericInput("obs", "Number of observations to view:", 10),
+
+      sliderInput("bins", "Number of bins:",
+                  min = 1,max = 50, value = 30)
     ),
 
-    # Show a plot of the generated distribution
+   # Show a summary of the dataset and an HTML table with the 
+   # requested number of observations
     mainPanel(
+      verbatimTextOutput("summary"), 
+      tableOutput("view"),
       plotOutput("distPlot")
     )
   )
